f40dca654c19bf6221cb9e4722c4c50d34ad5106
Jose Luis Duran <jlduran@FreeBSD.org>
2026-08-19 16:17:27 +0530

    nanobsd: Refactor cust_pkgng
    Move the existing function to the legacy plan, as it is
    incompatible with this implementation.
    
    Create a new cust_pkgng()
    
    Differential Revision:	https://reviews.freebsd.org/D58974

--- tools/tools/nanobsd/customizations.sh
+++ tools/tools/nanobsd/customizations.sh
@@ -132,21 +132,15 @@ cust_pkgng() {
 		if $do_clean; then
 			tgt_pkg install -F $NANO_PACKAGE_LIST
 		fi
-		mount -t nullfs -o noatime -o ro "$(nano_pkg_cachedir)" "${NANO_WORLDDIR}/var/cache/pkg"
-		trap "nano_umount ${NANO_WORLDDIR}/var/cache/pkg" 1 2 15 EXIT
 	fi
 
-	cp /etc/resolv.conf "${NANO_WORLDDIR}/etc/resolv.conf"
-	tgt_pkg_chroot install $NANO_PACKAGE_LIST
-	rm -f "${NANO_WORLDDIR}/etc/resolv.conf"
-	rm -rf "${NANO_WORLDDIR}/var/db/pkg/repos/"*
+	tgt_pkg install $NANO_PACKAGE_LIST
 
 	if [ -d "$NANO_PACKAGE_DIR" ]; then
 		trap - 1 2 15 EXIT
 		nano_umount "${NANO_WORLDDIR}/var/cache/pkg"
 	fi
 }
-
 
 #######################################################################
 # Patch adduser(8)
